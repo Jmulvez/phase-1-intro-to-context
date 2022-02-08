99186aa70dfefa3306ea25f9b31463de08526439
John <johnmulvey48@yahoo.com>
2022-02-08 11:08:23 -0500

    git@github.com:Jmulvez/phase-1-intro-to-context.git

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+employee = function createEmployeeRecord(firstName, familyName, title, payPerHour, timeInEvents, timeOutEvents) {
+    this.firstName = firstName;
+    this.familyName = familyName;
+    this.title = title;
+    this.payPerHour = payPerHour;
+    this.timeInEvents = timeInEvents;
+    this.timeOutEvents = timeOutEvents;
+}
+
+funciton createTimeInEvent(timeIn) {
+    return employee.timeInEvents;
+}
+
+function createTimeOutEvents(timeOut) {
+    return empployee.timeOutEvents;
+}
